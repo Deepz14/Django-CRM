1f695a15bf5bdc96eb3b23a1452cc52dc028c15d
Deepz14 <deepakchandrasekaran14@gmail.com>
2022-12-09 16:05:08 +0530

    Products model added

--- dj_crm/products/models.py
+++ dj_crm/products/models.py
@@ -1,3 +1,34 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
+
+    def __str__(self) -> str:
+        return self.name
+        
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
+
+    def __str__(self) -> str:
+        return self.name  
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField(max_length=200, null=True, blank=True)
+    category = models.ManyToManyField(Category, blank=True)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    stock = models.IntegerField(default=0)
+    rating = models.DecimalField(default=0, max_digits=3, decimal_places=1)
+    comment = models.TextField(max_length=50, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
+
+    def __str__(self) -> str:
+        return self.name
--- dj_crm/products/views.py
+++ dj_crm/products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
+def productsList(request):
     return render(request, 'products/products.html')
