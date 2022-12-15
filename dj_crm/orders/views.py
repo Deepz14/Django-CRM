832f97ae14f54799a4d3c5e192c87ef498598381
Deepz14 <deepakchandrasekaran14@gmail.com>
2022-12-15 14:50:27 +0530

    order return / cancel

--- dj_crm/orders/views.py
+++ dj_crm/orders/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from products.models import Product, Order
 
 # Create your views here.
+@login_required(login_url='login')
 def orderList(request):
     orders = Order.objects.all().order_by('-created_at')
     context = {'orders': orders}
@@ -14,5 +15,11 @@ def viewOrder(request):
 def updateOrder(request):
     pass
 
-def deleteOrder(request):
-    pass
+@login_required(login_url='login')
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    if request.method == "POST":
+        if order is not None:
+            order.delete()
+            return redirect('order-List')
+    return render(request, 'orders/delete_order.html', {'obj': order})
