11c0c9a724e69c8d66b6517f7bd9e1298091a26a
Deepak <deepakchandrasekaran14@gmail.com>
2022-12-21 14:53:19 +0530

    Update settings.py

--- dj_crm/dj_crm/settings.py
+++ dj_crm/dj_crm/settings.py
@@ -127,7 +127,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-# 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
 
 STATIC_URL = 'static/'
 MEDIA_URL = 'images/'
